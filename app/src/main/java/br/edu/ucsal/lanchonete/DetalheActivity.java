0e0b85d763e879c941665c46990a0c0a5cbeee94
azurah <byazura@live.com>
2018-03-23 10:06:35 -0300

    Lanchonete pre-alpha 0.0.0.1

--- app/src/main/java/br/edu/ucsal/lanchonete/DetalheActivity.java
+++ app/src/main/java/br/edu/ucsal/lanchonete/DetalheActivity.java
@@ -1,7 +1,9 @@
 package br.edu.ucsal.lanchonete;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalheActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DetalheActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+        String nome = getIntent().getStringExtra("textProdutos");
+        TextView produto = findViewById(R.id.produto);
+        produto.setText(nome);
+
+
     }
 }
